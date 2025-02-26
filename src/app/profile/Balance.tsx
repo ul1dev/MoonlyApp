b8cb2812994d491bb6b52626cfcbc5664ff6f6c3
Igor0400 <ulanov.igor0400@gmail.com>
2025-02-26 12:31:02 +0300

    made profile btns

--- src/app/profile/Balance.tsx
+++ src/app/profile/Balance.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useMediaQuery } from '../common/hooks/use-media-query';
 import BoostsIcon from '../common/share/icons/Boosts';
 import PixelGreenCoinIcon from '../common/share/icons/PixelGreenCoin';
@@ -45,38 +46,65 @@ export default function UserBalance() {
                     </p>
                 </div>
             </div>
-            <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
-                <p className="text-[#B2B2B2] font-medium">Boosts</p>
-                <div className="flex gap-[3px] items-center">
-                    <p className="text-white font-bold">100</p>
-                    <div className="max-[680px]:-mt-[1px] -mt-0.5">
-                        <BoostsIcon width={boostWidth} height={iconsSize} />
+            <div className="flex justify-between gap-3 items-center">
+                <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
+                    <p className="text-[#B2B2B2] font-medium">Boosts</p>
+                    <div className="flex gap-[3px] items-center">
+                        <p className="text-white font-bold">100</p>
+                        <div className="max-[680px]:-mt-[1px] -mt-0.5">
+                            <BoostsIcon width={boostWidth} height={iconsSize} />
+                        </div>
                     </div>
                 </div>
+                <Link href="/shop">
+                    <div className="border-2 border-white rounded-3xl cursor-pointer -mt-1">
+                        <p className="max-[380px]:text-xs max-[500px]:text-sm max-[680px]:text-[15px] text-base font-bold max-[440px]:px-5 max-[680px]:py-1 px-6 py-[5px] text-white">
+                            Buy
+                        </p>
+                    </div>
+                </Link>
             </div>
-            <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
-                <p className="text-[#B2B2B2] font-medium">Coins</p>
-                <div className="flex gap-[3px] items-center">
-                    <p className="text-white font-bold">100</p>
-                    <div className="max-[680px]:-mt-[1px] -mt-0.5">
-                        <PixelGreenCoinIcon
-                            width={iconsSize}
-                            height={iconsSize}
-                        />
+            <div className="flex justify-between gap-3 items-center">
+                <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
+                    <p className="text-[#B2B2B2] font-medium">Coins</p>
+                    <div className="flex gap-[3px] items-center">
+                        <p className="text-white font-bold">100</p>
+                        <div className="max-[680px]:-mt-[1px] -mt-0.5">
+                            <PixelGreenCoinIcon
+                                width={iconsSize}
+                                height={iconsSize}
+                            />
+                        </div>
                     </div>
                 </div>
+                <Link href="/shop">
+                    <div className="border-2 border-white rounded-3xl cursor-pointer -mt-1">
+                        <p className="max-[380px]:text-xs max-[500px]:text-sm max-[680px]:text-[15px] text-base font-bold max-[440px]:px-5 max-[680px]:py-1 px-6 py-[5px] text-white">
+                            Buy
+                        </p>
+                    </div>
+                </Link>
             </div>
-            <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
-                <p className="text-[#B2B2B2] font-medium">Points</p>
-                <div className="flex gap-[3px] items-center">
-                    <p className="text-white font-bold">100</p>
-                    <div className="max-[680px]:-mt-[1px] -mt-0.5">
-                        <PixelPurpleCoinIcon
-                            width={iconsSize}
-                            height={iconsSize}
-                        />
+            <div className="flex justify-between gap-3 items-center">
+                <div className="flex gap-[10px] max-[380px]:text-base max-[440px]:text-[18px] max-[680px]:text-[20px] text-[22px] items-center">
+                    <p className="text-[#B2B2B2] font-medium">Points</p>
+                    <div className="flex gap-[3px] items-center">
+                        <p className="text-white font-bold">100</p>
+                        <div className="max-[680px]:-mt-[1px] -mt-0.5">
+                            <PixelPurpleCoinIcon
+                                width={iconsSize}
+                                height={iconsSize}
+                            />
+                        </div>
                     </div>
                 </div>
+                <Link href="/">
+                    <div className="border-2 border-white rounded-3xl cursor-pointer -mt-1">
+                        <p className="max-[380px]:text-xs max-[500px]:text-sm max-[680px]:text-[15px] text-base font-bold max-[440px]:px-5 max-[680px]:py-1 px-6 py-[5px] text-white">
+                            Farm
+                        </p>
+                    </div>
+                </Link>
             </div>
         </div>
     );
