d7517d712833d4bb6f287d966be4269d0a57f95a
Igor0400 <ulanov.igor0400@gmail.com>
2025-03-11 12:13:37 +0300

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+    {
+        rules: {
+            // Отключаем базовое правило ESLint
+            'prefer-const': 'off',
+            // Отключаем все правила TypeScript
+            ...Object.fromEntries(
+                Object.keys(
+                    await import('@typescript-eslint/eslint-plugin').then(
+                        (m) => m.rules
+                    )
+                ).map((rule) => [`@typescript-eslint/${rule}`, 'off'])
+            ),
+        },
+    },
 ];
 
 export default eslintConfig;
