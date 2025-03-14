f2faaef53c3a9f8cf87d02a3848919bd628662c6
Igor0400 <ulanov.igor0400@gmail.com>
2025-03-14 19:20:58 +0300

    commit

--- src/app/common/locale/nft.ts
+++ src/app/common/locale/nft.ts
@@ -2,11 +2,11 @@ export const nftTexts = {
     en: {
         soon: 'Soon...',
         soonDescr:
-            'This section will be a game resulting in your own unique nft, which you can then sell on our domestic or foreign markets, or build up your collection.',
+            'This section will be a game that turns your game wins into exclusive NFTs - each token is unique and is created based on your real-time actions. Your NFTs can then be sold on our internal or other exchanges, or you can build your NFT collection.',
     },
     ru: {
         soon: 'Скоро...',
         soonDescr:
-            'В этом разделе будет игра, в результате которой вам будет генерироваться свой собственный уникальный nft, который затем сможете продать на нашей внутренней или на других биржах или собирать свою nft коллекцию.',
+            'В этом разделе будет игра, которая превращает ваши игровые победы в эксклюзивные NFT – каждый токен уникален и создается на основе ваших действий в реальном времени. Свои NFT затем вы сможете продавать на нашей внутренней или на других биржах или собирать свою NFT-коллекцию.',
     },
 };
